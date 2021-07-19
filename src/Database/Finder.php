2c97e66c40d64bf76a3f6db3ef3849f12d51f461
Gergely Laborci <gergely@laborci.hu>
2021-07-19 15:28:26 +0200

    group by and having added

--- src/Database/Finder.php
+++ src/Database/Finder.php
@@ -11,20 +11,26 @@ class Finder {
 	private ?Filter $filter = null;
 	private string $from;
 	private array $order = [];
+	private array $groupBy = [];
+	private ?Filter $having = null;
 
 	public function __construct(private Connection $connection) { }
 	public function cache(int $sec): static {
 		$this->cacheInterval = $sec;
 		return $this;
 	}
-	public function fields(string ...$select): static {
-		$select = array_map(function ($field): string { return $this->connection->escape($field); }, $select);
-		return $this->select(join(',', $select));
+	public function fields(string ...$fields): static {
+		$fields = array_map(function ($field): string { return $this->connection->escape($field); }, $fields);
+		return $this->select(join(',', $fields));
 	}
 	public function select(string|null $select = null): static {
 		if (is_null($select)) $this->select = [];
 		else $this->select[] = $select;
 		return $this;
+	}
+
+	public function groupBy(string ...$fields) {
+		$this->groupBy = array_map(function ($field): string { return $this->connection->escape($field); }, $fields);
 	}
 
 	public function table(string $from): static {
@@ -33,6 +39,14 @@ class Finder {
 
 	public function from(string $from): static {
 		$this->from = $from;
+		return $this;
+	}
+	public function having(null|Filter $filter): static {
+		if (is_null($this->having)) {
+			$this->having = $filter;
+		} else {
+			$this->having->and($filter);
+		}
 		return $this;
 	}
 	public function where(null|Filter $filter): static {
@@ -78,7 +92,8 @@ class Finder {
 				if ($count !== false) $count = $this->connection->query('SELECT FOUND_ROWS()')->fetch(\PDO::FETCH_COLUMN);
 				$item->expiresAfter($this->cacheInterval);
 				return ['records' => $records, 'count' => $count];
-			});
+			})
+			;
 			$records = $cached['records'];
 			$count = $cached['count'];
 		} else {
@@ -96,6 +111,8 @@ class Finder {
 			(count($this->select) ? join(',', $this->select) : '*') . ' ' .
 			' FROM ' . $this->from . ' ' .
 			($this->filter != null && !is_null($filter = $this->filter->getSql($this->connection)) ? ' WHERE ' . $filter . ' ' : '') .
+			(count($this->groupBy) ? 'GROUP BY '.join(',', $this->groupBy).' ' :'')
+			($this->having != null && !is_null($having = $this->having->getSql($this->connection)) ? ' HAVING ' . $having . ' ' : '') .
 			(count($this->order) ? ' ORDER BY ' . join(', ', $this->order) : '') .
 			($limit ? ' LIMIT ' . $limit : '') .
 			($offset ? ' OFFSET ' . $offset : '');
