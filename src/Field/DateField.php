b4d0901f783d5a1135727b2d0ff99eb25d665970
Gergely Laborci <gergely@laborci.hu>
2021-09-29 16:08:43 +0200

    date fix

--- src/Field/DateField.php
+++ src/Field/DateField.php
@@ -3,13 +3,13 @@
 use Atomino\Carbon\Field\Attributes\FieldDescriptor;
 use Symfony\Component\Validator\Constraints\Type;
 
-#[FieldDescriptor('\\'.\DateTime::class, null)]
-class DateField extends Field{
-	public function build(mixed $value){ return is_null($value) ? null : new \DateTime($value); }
+#[FieldDescriptor('\\' . \DateTime::class, null)]
+class DateField extends Field {
+	public function build(mixed $value) { return is_null($value) ? null : new \DateTime($value); }
 	/** @param \DateTime $value */
-	public function store(mixed $value){ return is_null($value) ? null : $value->format('Y-m-d'); }
-	public function import(mixed $value){ return is_null($value) ? null : \DateTime::createFromFormat(\DateTime::ISO8601, $value); }
+	public function store(mixed $value) { return is_null($value) ? null : $value->format('Y-m-d'); }
+	public function import(mixed $value) { return is_null($value) ? null : new \DateTime($value); }
 	/** @param \DateTime $value */
-	public function export(mixed $value){ return is_null($value) ? null : $value->format(\DateTime::ISO8601); }
+	public function export(mixed $value) { return is_null($value) ? null : $value->format(\DateTime::ATOM); }
 
 }
--- src/Field/DateTimeField.php
+++ src/Field/DateTimeField.php
@@ -5,8 +5,8 @@ use Atomino\Carbon\Field\Attributes\FieldDescriptor;
 class DateTimeField extends Field{
 	public function build(mixed $value){ return is_null($value) ? null : new \DateTime($value); }
 	/** @param \DateTime $value */
-	public function store(mixed $value){ return is_null($value) ? null : $value->format('Y-m-d H:i:s'); }
-	public function import(mixed $value){ return is_null($value) ? null : \DateTime::createFromFormat(\DateTime::ISO8601, $value); }
+	public function store(mixed $value){ return is_null($value) ? null : $value->setTimezone(new \DateTimeZone('UTC'))->format('Y-m-d H:i:s'); }
+	public function import(mixed $value){ return is_null($value) ? null : \DateTime::createFromFormat(\DateTimeInterface::ISO8601, $value); }
 	/** @param \DateTime $value */
-	public function export(mixed $value){ return is_null($value) ? null : $value->format(\DateTime::ISO8601); }
+	public function export(mixed $value){ return is_null($value) ? null : $value->format(\DateTimeInterface::ISO8601); }
 }
