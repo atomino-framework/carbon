2d71ae22d2fafc41156f1a7135c4950f73e73adb
Gergely Laborci <gergely@laborci.hu>
2021-10-29 18:56:25 +0200

    minor fixes

--- src/Database/Finder/Filter.php
+++ src/Database/Finder/Filter.php
@@ -2,7 +2,7 @@
 
 use Atomino\Carbon\Database\Connection;
 
-class Filter{
+class Filter {
 
 	const AND = 1;
 	const OR = 2;
@@ -10,52 +10,52 @@ class Filter{
 
 	protected array $where = [];
 
-	protected function __construct(){ }
+	protected function __construct() { }
 
-	static public function where(Comparison|Filter|string|false $sql = false, ...$sqlParams): static{
+	static public function where(Comparison|Filter|string|false|null $sql = false, ...$sqlParams): static {
 		$filter = new static();
 		return $filter->addWhere(static::AND, $sql, $sqlParams);
 	}
 
-	public function and(Comparison|Filter|string|bool $sql, ...$args): static{ return $this->addWhere(static:: AND, $sql, $args); }
-	public function or(Comparison|Filter|string|bool $sql, ...$args): static{ return $this->addWhere(static:: OR, $sql, $args); }
-	public function andNot(Comparison|Filter|string|bool $sql, ...$args): static{ return $this->addWhere(static:: AND + static::NOT, $sql, $args); }
-	public function orNot(Comparison|Filter|string|bool $sql, ...$args): static{ return $this->addWhere(static:: OR + static::NOT, $sql, $args); }
+	public function and(Comparison|Filter|string|bool|null $sql, ...$args): static { return $this->addWhere(static:: AND, $sql, $args); }
+	public function or(Comparison|Filter|string|bool|null $sql, ...$args): static { return $this->addWhere(static:: OR, $sql, $args); }
+	public function andNot(Comparison|Filter|string|bool|null $sql, ...$args): static { return $this->addWhere(static:: AND + static::NOT, $sql, $args); }
+	public function orNot(Comparison|Filter|string|bool|null $sql, ...$args): static { return $this->addWhere(static:: OR + static::NOT, $sql, $args); }
 
-	protected function addWhere(int $type, Comparison|Filter|string|bool $sql, array $args): static{
-		if ($sql !== false) $this->where[] = ['type' => $type, 'sql' => $sql, 'args' => $args];
+	protected function addWhere(int $type, Comparison|Filter|string|bool|null $sql, array $args): static {
+		if ($sql !== false && $sql !== null) $this->where[] = ['type' => $type, 'sql' => $sql, 'args' => $args];
 		return $this;
 	}
 
 
-	public function getSql(Connection $connection): ?string{
+	public function getSql(Connection $connection): ?string {
 		if (!count($this->where)) return null;
 		$sql = '';
-		foreach ($this->where as $segment){
-			if ($segment['sql'] instanceof Comparison){
+		foreach ($this->where as $segment) {
+			if ($segment['sql'] instanceof Comparison) {
 				$where = $segment['sql']->getSql($connection);
-			}elseif ($segment['sql'] instanceof Filter){
+			} elseif ($segment['sql'] instanceof Filter) {
 				$where = $segment['sql']->getSql($connection);
-			}elseif (is_array($segment['sql'])){
+			} elseif (is_array($segment['sql'])) {
 				$where = $this->getSqlFromArray($segment['sql'], $connection);
-			}else{
+			} else {
 				$where = $connection->getSmart()->applySQLArguments($segment['sql'], $segment['args']);
 			}
 
-			if (trim($where)){
-				if (strlen($sql) > 0){
+			if (trim($where)) {
+				if (strlen($sql) > 0) {
 					$sql .= $segment['type'] & self:: AND ? ' AND ' : ' OR ';
 				}
-				$sql .= ( $segment['type'] & self::NOT ? ' NOT ' : '' ) . "(" . $where . ")";
+				$sql .= ($segment['type'] & self::NOT ? ' NOT ' : '') . "(" . $where . ")";
 			}
 		}
 		return $sql;
 	}
 
-	protected function getSqlFromArray(array $filter, Connection $connection): ?string{
+	protected function getSqlFromArray(array $filter, Connection $connection): ?string {
 		if (!$filter) return null;
 		$sql = [];
-		foreach ($filter as $key => $value){
+		foreach ($filter as $key => $value) {
 			$sql[] = $connection->escape($key) . (
 				is_array($value) ?
 					' IN (' . array_map(fn($arg) => $connection->quote($arg), $value) . ')' :
@@ -64,7 +64,6 @@ class Filter{
 		}
 		return implode(' AND ', $sql);
 	}
-
 
 
 }
--- src/Finder.php
+++ src/Finder.php
@@ -4,44 +4,50 @@ use Atomino\Carbon\Database\Connection;
 use Symfony\Component\Cache\CacheItem;
 use Symfony\Contracts\Cache\CacheInterface;
 
-class Finder extends \Atomino\Carbon\Database\Finder{
+class Finder extends \Atomino\Carbon\Database\Finder {
 
 	private string $entity;
 	private ?CacheInterface $cache;
 
-	public function __construct(Connection $connection, private Model $model){
+	public function __construct(Connection $connection, private Model $model) {
 		parent::__construct($connection);
 		$this->table($model->getTable());
 		$this->entity = $model->getEntity();
 		$this->cache = $model->getCache();
 	}
 
-	public function pick(): ?Entity{
+	public function pick(): ?Entity {
 		$items = $this->collect(1);
 		return array_pop($items);
 	}
 
-	public function count():int{
+	public function count(): int {
 		$this->select("count(*) as `count`");
 		return parent::record()['count'];
 	}
 
 	/** @return Entity[] */
-	public function page(int $size, int $page = 1, int|bool|null &$count = false): array{
-		return $this->collect($size, $size * ( $page - 1 ), $count);
+	public function page(int $size, int &$page = 1, int|bool|null &$count = false, $handleOverflow = true): array {
+		if ($page < 1) $page = 1;
+		$items = $this->collect($size, $size * ($page - 1), $count);
+		if (count($items) === 0 && $handleOverflow && $page !== 1) {
+			$page = max(1, ceil($count / $size));
+			$items = $this->collect($size, $size * ($page - 1), $count);
+		}
+		return $items;
 	}
 
 	/** @return Entity[] */
-	public function collect(?int $limit = null, ?int $offset = null, int|bool|null &$count = false): array{
+	public function collect(?int $limit = null, ?int $offset = null, int|bool|null &$count = false): array {
 		$items = [];
 
 		$records = parent::records($limit, $offset, $count);
 
 		/** @var \Atomino\Carbon\Entity $entity */
 		$entity = $this->entity;
-		foreach ($records as $record){
+		foreach ($records as $record) {
 			$items[] = $entity::build($record);
-			$this->cache->get($this->model->generateCacheKey($record['id']), function (CacheItem $item) use ($record){ return $record; });
+			$this->cache->get($this->model->generateCacheKey($record['id']), function (CacheItem $item) use ($record) { return $record; });
 		}
 
 		return $items;
